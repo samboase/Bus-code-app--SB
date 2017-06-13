bb79a60253f920571df8bdbe7725488b0d3784d2
SamBoase <samboase@students.freyberg.ac.nz>
2017-06-13 12:30:02 +1200

    added all objects

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //CLASS
 class Bus {
 
-    Bus (name, stops, colour, monFriTimes, friTimes) {
+    Constuctor (name, stops, colour, monFriTimes, friTimes) {
         this.name = name;
         this.stops = stops;
         this.monFriTimes = monFriTimes;
@@ -19,9 +19,82 @@ class Bus {
 
 
 //OBJECTS
-var awapuni = new Bus ("Awapuni", awapuniStops, "#f69238", awapuniTimesMonFri, awapuniTimesFriOnly)
+var awapuni = new Bus ("Awapuni",
+                       awapuniStops,
+                       "#f69238",
+                       awapuniTimesMonFri,
+                       awapuniTimesFriOnly,
+                       awapuniTimesSat,
+                       awapuniTimesSun)
 
-var rugby = new Bus ("Rugby", rugbyStops, "#009e57", rugbyTimesMonFri, rubgyTimesFri  )
+var rugby = new Bus ("Rugby",
+                     rugbyStops,
+                     "#009e57",
+                     rugbyTimesMonFri,
+                     rubgyTimesFri,
+                     rugbyTimesSat,
+                     rugbyTimesSun)
+
+var highbury = new Bus ("Highbury",
+                        highburyStops,
+                        "#00aeef",
+                        highburyTimesMonFri,
+                        highburyFridayEveningTimes)
+
+var takaro = new Bus ("Takaro",
+                      takaroStops,
+                      "#ee1b2d",
+                      takaroTimesMonFri,
+                      takaroTimesFri,
+                      takaroTimesSat,
+                      takaroTimesSun)
+
+var cloverlea = new Bus ("Cloverlea",
+                         cloverleaStops,
+                         "#873c96",
+                         cloverleaTimesMonFri,
+                         cloverleaTimesFriOnly,
+                         cloverleaTimesSat,
+                         cloverleaTimesSun)
+
+var milson = new Bus ("Milson",
+                      milsonStops,
+                      "#ef59a1",
+                      milsonTimesMonFri,
+                      milsonTimesFri,
+                      milsonTimesSat,
+                      milsonTimesSun)
+
+var rhodes = new Bus ("Rhodes",
+                      rhodesStops,
+                      "#0153a0",
+                      rhodesTimesMonFri,
+                      '')
+
+var roslyn = new Bus ("Roslyn",
+                      roslynStops,
+                      "#c6870e",
+                      roslynTimesMonFri,
+                      roslynTimesFriOnly)
+
+var brightwater = new Bus ("Brightwater",
+                           brightwaterStops,
+                           "#fbb555",
+                           brightwaterTimesMonFri,
+                           brightwaterTimesFri )
+
+var fernlea = new Bus ("Fernlea",
+                       fernleaStops,
+                       "#008b98",
+                       fernleaTimesMonFri,
+                       fernleaTimesFri)
+
+var heights = new Bus ("Heights",
+                       heightsStops,
+                       "#f0563b",
+                       heightsTimesMonFri,
+                       heightsTimesFri)
+
 
 //JQUERY STUFF
 $(document).ready(function(){
